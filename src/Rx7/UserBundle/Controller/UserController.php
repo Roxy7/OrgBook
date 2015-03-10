dc4a193343e9a68afccd57ce028b914dc3812a2a
Roxy7 <Roxanne7_@collectifembriagoun.com>
2015-03-10 16:23:59 +0100

    Creation du cartouche de gestion utilisateur

---
+++ src/Rx7/UserBundle/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Rx7\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Rx7\UserBundle\Entity\User;
+
+
+use JMS\SecurityExtraBundle\Annotation\Secure;
+
+
+class UserController extends Controller
+{
+	
+	public function navLinkAction ()
+	{
+		$links = array( array('route' => 'fos_user_profile_edit', 'display' => 'Editer le profil'),
+						array('route' => 'fos_user_change_password', 'display' => 'Changer de mdp')
+		);
+		
+		return $this->render('Rx7UserBundle::navLink.html.twig', array(
+				'link_list' => $links
+		));
+	}
+	
+}
